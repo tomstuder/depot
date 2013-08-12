cbbda3665846e9b319623dc3f5f55c551f59dc94
tstuderTOPIC <tstuder@topicdesign.com>
2013-08-12 12:07:42 -0400

    initial commit, added products scaffold, edited form field, edited migration file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'bc00d2d29e52d954765b8287e9768f14447ed8fa9103bca2bad109d8fbdfa200e5ac1c0670cb4615348291822e61409ded6ba83e012bd1f64e4356bda8e732fd'
